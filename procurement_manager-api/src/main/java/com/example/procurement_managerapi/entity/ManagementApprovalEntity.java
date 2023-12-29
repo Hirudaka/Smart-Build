e2c19ce9769d9b71c2eb400a997302ee2a2c30e0
Hirudaka Kumarasinghe <it21304538@my.sliit.lk>
2023-12-29 22:07:22 +0530

    Update ManagementApprovalEntity.java

--- procurement_manager-api/src/main/java/com/example/procurement_managerapi/entity/ManagementApprovalEntity.java
+++ procurement_manager-api/src/main/java/com/example/procurement_managerapi/entity/ManagementApprovalEntity.java
@@ -1,4 +1,12 @@
+package com.example.procurement_managerapi.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor; 
+
+import java.util.Date;
 
 @Entity
 @Data
