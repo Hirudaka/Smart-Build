ebef89850747bd92dfc15daf8974535681d58bd9
Hirudaka Kumarasinghe <it21304538@my.sliit.lk>
2023-12-31 23:57:18 +0530

    Update ApprovalItem.java

--- procurement_manager-api/src/main/java/com/example/procurement_managerapi/model/ApprovalItem.java
+++ procurement_manager-api/src/main/java/com/example/procurement_managerapi/model/ApprovalItem.java
@@ -2,7 +2,7 @@ package com.example.procurement_managerapi.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.NoArgsConstructor
 
 import java.util.Date;
 
