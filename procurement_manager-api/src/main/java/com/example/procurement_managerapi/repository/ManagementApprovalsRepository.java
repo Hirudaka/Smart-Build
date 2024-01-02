6244ac9b07dff5be5aafe2f1f08cf754b76076b7
Hirudaka Kumarasinghe <it21304538@my.sliit.lk>
2024-01-02 19:38:25 +0530

    Update ManagementApprovalsRepository.java

--- procurement_manager-api/src/main/java/com/example/procurement_managerapi/repository/ManagementApprovalsRepository.java
+++ procurement_manager-api/src/main/java/com/example/procurement_managerapi/repository/ManagementApprovalsRepository.java
@@ -1,7 +1,6 @@
 package com.example.procurement_managerapi.repository;
 
 import com.example.procurement_managerapi.entity.ManagementApprovalEntity;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
