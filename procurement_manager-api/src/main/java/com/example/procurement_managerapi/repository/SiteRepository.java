8c0268518e10f00878c1b0ebbf08e40d0da7d3fb
Hirudaka Kumarasinghe <it21304538@my.sliit.lk>
2024-01-20 22:49:08 +0530

    Update SiteRepository.java

--- procurement_manager-api/src/main/java/com/example/procurement_managerapi/repository/SiteRepository.java
+++ procurement_manager-api/src/main/java/com/example/procurement_managerapi/repository/SiteRepository.java
@@ -1,9 +1,1 @@
-package com.example.procurement_managerapi.repository;
 
-import com.example.procurement_managerapi.entity.SiteEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SiteRepository extends JpaRepository<SiteEntity,Long> {
-}
